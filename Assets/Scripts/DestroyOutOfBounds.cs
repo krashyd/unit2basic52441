af73762073f892dc541dfc95b3940e6995886936
krashyd <a52441@alunos.ipb.pt>
2024-04-25 15:47:33 +0100

    Destroy animals offscreen
    a52441

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,8 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound){
             Destroy(gameObject);
         }
     }
