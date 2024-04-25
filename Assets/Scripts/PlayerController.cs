9527e7815e7a9f4686bb923e0c6d528ef482de69
krashyd <a52441@alunos.ipb.pt>
2024-04-25 15:35:38 +0100

    Launch projectile from spacebar
    a52441

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //keep the player in bounds
         if (transform.position.x < -xRange)
          {
@@ -28,7 +29,15 @@ public class PlayerController : MonoBehaviour
          {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
          }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+         }
     }
+
 }
