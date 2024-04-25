39a042f2d7202c8587e45ab9f158968af6862d68
krashyd <a52441@alunos.ipb.pt>
2024-04-25 16:04:04 +0100

    Spawn random animals, location and camera change
    a52441

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,10 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if( Input.GetKeyDown(KeyCode.S))
+        //randomly generate animal index and spawn position
         {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
